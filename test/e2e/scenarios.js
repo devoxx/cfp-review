68b96734c039f8743b666c372c4e9e1dd081ec1a
clakech <cyril.lakech@adeoservices.com>
2013-05-15 11:47:50 +0200

    reduce presentation.json size... too big

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -11,7 +11,7 @@ describe('Main', function () {
   describe('Display presentations', function () {
     it('should display a list of presentation with only one selected', function () {
       expect(element('[id^=presentation-]').count()).toBeGreaterThan(1);
-      expect(element('[id^=presentation-][class*=success]').count()).toEqual(194);
+      expect(element('[id^=presentation-][class*=success]').count()).toEqual(7);
     });
   });
 
